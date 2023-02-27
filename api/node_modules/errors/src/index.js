c388622af3ba3ae1f43d871bfe308e5a4e9e1ed6
mgblanco10 <mgblanco1001@gmail.com>
2023-02-27 12:42:19 +0100

    testing in case I have to show it in the interview

---
+++ api/node_modules/errors/src/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    DuplicityError: require('./DuplicityError'),
+    SystemError: require('./SystemError'),
+    UnknownError: require('./UnknownError'),
+    FormatError: require('./FormatError'),
+    AuthError: require('./AuthError'),
+    NotFoundError: require('./NotFoundError'),
+    ClientError: require('./ClientError'),
+    ServerError: require('./ServerError'),
+    ConflictError: require('./ClientError')   
+}
